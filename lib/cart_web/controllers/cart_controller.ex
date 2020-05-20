ea4460e3bbfefeb957230ffe00259bc590523fb7
jphrubant <jphrubant@gmail.com>
2020-05-20 18:35:54 +0200

    edit/update handlers setup

--- lib/cart_web/controllers/cart_controller.ex
+++ lib/cart_web/controllers/cart_controller.ex
@@ -9,8 +9,8 @@ defmodule CartWeb.CartController do
     render(conn, "index.html", cart: cart)
   end
 
-  def new(conn, params) do
-    changeset = Cart.changeset(%Cart{}, %{}) # === Cart.changeset("struct", params)
+  def new(conn, _params) do
+    changeset = Cart.changeset(%Cart{}, %{}) # === Cart.changeset(struct, params)
     render(conn, "new.html", changeset: changeset)
   end
 
@@ -24,6 +24,27 @@ defmodule CartWeb.CartController do
           |> redirect(to: Routes.cart_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def edit(conn, %{"id" => cart_id}) do
+    cart = Repo.get(Cart, cart_id)
+    changeset = Cart.changeset(cart, %{})
+
+    render(conn, "edit.html", changeset: changeset, cart: cart)
+  end
+
+  def update(conn, %{"cart" => cart, "id" => id}) do
+    old_cart = Repo.get(Cart, id)
+    changeset = Cart.changeset(old_cart, cart)
+
+    case Repo.update(changeset) do
+      {:ok, _cart} ->
+        conn
+          |> put_flash(:info, "Cart Updated")
+          |> redirect(to: Routes.cart_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset, cart: old_cart)
     end
   end
 
